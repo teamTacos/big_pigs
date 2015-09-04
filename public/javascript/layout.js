1b8a84ccc31c82b475a5021a7d1e50b063f3080b
Jarod Adair <jarod.adair@manheim.com>
2015-09-04 17:07:06 -0500

    adding initial bootstrap styling layout

---
+++ public/javascript/layout.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.available-game').click(function() {
+        $('.join-room').show();
+    });
+    $('.cancel-join').click(function() {
+        $('.join-room').hide();
+    });
+    $('#join-room').click(function(){
+
+    })
+});
