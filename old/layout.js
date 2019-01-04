dcdaeda343fd8a7303fdef50e0a02dcb8df71035
Jarod Adair <adairjk@gmail.com>
2019-01-04 14:35:52 -0600

    cleaning things up a bit and set player name when joining

--- old/layout.js
+++ old/layout.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
     $('.available-game').click(function() {
         $('.join-room').show();
---
+++ public/javascript/room.js
@@ -0,0 +1,48 @@
+var socket = io.connect('http://localhost:3000');
+// var socket = io.connect('big-pigs:server');
+var playerList;
+
+function byId(player) {
+    return player.id === socket.id;
+}
+
+var Message = function(player) {
+	this.player = player,
+	this.text = ''
+}
+
+$('form').submit(function(){
+    console.log(socket.id);
+    var player = playerList.find(byId);
+    message = new Message(player);
+    message.text = $('#m').val();
+    socket.emit('chat message', message);
+    $('#m').val('');
+    return false;
+});
+
+socket.on('chat message', function(msg){
+    $('#messages').append($('<li><strong>' + msg.player.name + ':</strong> ' + msg.text + '</li>'));
+    $('.chat-list-container').animate({
+        scrollTop: $('#messages li:last-child').offset().top + 'px'
+    }, 1000);
+});
+
+socket.on('player list', function(players){
+    $('#player-list').empty();
+    playerList = players;
+    players.forEach(function(player){
+       if (player.turn === true) {
+           $('#player-list').append('<li class="list-group-item active"><h4 class="name">' + player.name + '</h4><span class="badge">' + player.score + '</span></li>')
+       } else {
+           $('#player-list').append('<li class="list-group-item"><h4 class="name">' + player.name + '</h4><span class="badge">' + player.score + '</span></li>')
+       }
+    });
+});
+
+$('#joinGame').click(function(){
+    name = $('#playerName').val();
+    //joinRoom(name);
+    socket.emit('join game', name);
+    $('.overlay').hide();
+});
