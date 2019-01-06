463b1fa1fb0939f93ec30fc291de94832ad58b20
Jarod Adair <adairjk@gmail.com>
2019-01-05 22:25:33 -0600

    test multiple players joining the game

--- test/features/join_game.feature
+++ test/features/join_game.feature
@@ -1,5 +1,9 @@
 Feature: Players should be able to join a game
 
 	Scenario: Player name should be added to list of players after joining
-		When a new player joings the game
+		When a new player joins the game
 		Then I should see that new player is in the list of players
+
+	Scenario: Mulitple players should be able to join the game
+		When 3 players have joined the game
+		Then I should see each of the player names in the list of players
--- test/features/step_definitions/game_steps.rb
+++ test/features/step_definitions/game_steps.rb
@@ -1,4 +1,4 @@
-When("a new player joings the game") do
+When("a new player joins the game") do
   @big_pigs = BigPigs.new
   @big_pigs.main_page.new_player_name.set('Player 1')
   @big_pigs.main_page.join_room.click
@@ -6,4 +6,22 @@ end
 
 Then("I should see that new player is in the list of players") do
   expect(@big_pigs.main_page.player_list.text).to include 'Player 1'
+end
+
+When("{int} players have joined the game") do |player_count|
+	@browsers = []
+	player_count.times do |num|
+		big_pigs = BigPigs.new
+		big_pigs.main_page.new_player_name.set("Player #{num}")
+  		big_pigs.main_page.join_room.click
+  		@browsers << big_pigs
+	end	
+end
+
+Then("I should see each of the player names in the list of players") do
+  	@browsers.each_with_index do |browser|
+  		0.upto(@browsers.count - 1) do |index|
+  			expect(browser.main_page.player_list.text).to include "Player #{index}"
+		end
+  	end
 end
